5b0f56e3f0288c352c70631dc14ee26ad7baa110
Siddharth Chinchwadkar <siddharthc.2310@gmail.com>
2025-08-05 10:29:02 +0530

    Update entire ecommerce-frontend project

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,10 @@
 console.log("E-Commerce Website Loaded");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("mobile-nav");
+  });
+});
